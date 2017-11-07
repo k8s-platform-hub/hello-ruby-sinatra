cb40144598486f86a437e4552f8fd98e62bc6132
Tirumarai Selvan <tiru@hasura.io>
2017-11-07 17:06:02 +0530

    add environment and get_articles endpoint

--- microservices/app/src/app.rb
+++ microservices/app/src/app.rb
@@ -1,8 +1,36 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
 
-set :bind, '0.0.0.0'
-set :port, 8080
+configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 8080
+  set :DATA_URL, 'http://127.0.0.1:6432/v1/query'
+end
+
+configure :production do
+  set :DATA_URL, 'http://data.hasura/v1/query'
+end
 
 get '/' do
   "Hello Sinatra"
+end
+
+get '/get_articles' do
+  query = {
+      'type' => 'select',
+      'args' => {
+          'table' => 'article',
+          'columns' => [
+              '*'
+          ]
+      }
+  }
+  uri = URI(settings.DATA_URL)
+  req = Net::HTTP::Post.new(uri)
+  req.body = query.to_json
+  req.content_type = 'application/json'
+  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    http.request(req)
+  end
 end
